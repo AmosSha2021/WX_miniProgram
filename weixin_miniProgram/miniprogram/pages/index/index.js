7006250ca312caba9537e35ad54aa53c05016466
Amos_Sha <116366105@qq.com>
2025-03-20 23:45:33 +0800

    add activity type for sign up in main page

--- weixin_miniProgram/miniprogram/pages/index/index.js
+++ weixin_miniProgram/miniprogram/pages/index/index.js
@@ -4,11 +4,64 @@ Page({
    * 页面的初始数据
    */
   data: {
-    userInfo: null // 确保已添加该数据字段
+    userInfo: null, // 确保已添加该数据字段
+    activityTypes: ['爬山', '篮球', '足球', '羽毛球', '乒乓球','马拉松'],
+    selectedActivityType: null,
   },
-  handle_signup(){
-    console.log(this.data.userInfo)
+  // 新增活动类型选择处理
+  bindActivityTypeChange(e) {
+    this.setData({
+      selectedActivityType: e.detail.value
+    })
   },
+  // 修改提交方法
+  async submitRegistration() {
+    const { userInfo } = getApp().globalData;
+    const { selectedActivityType, activityTypes } = this.data;
+    
+    if (!selectedActivityType) {
+      wx.showToast({ title: '请选择活动类型', icon: 'none' });
+      return;
+    }
+  
+    try {
+      // 新增校验查询
+      const checkRes = await wx.cloud.database().collection('activity_register')
+        .where({
+          employeeID: userInfo.employeeID,
+          activity_type: activityTypes[selectedActivityType]
+        })
+        .count();
+  
+      if (checkRes.total > 0) {
+        wx.showModal({
+          title: '提示',
+          content: '您已报名过该活动',
+          showCancel: false
+        });
+        return;
+      }
+  
+      // 原上传逻辑
+      await wx.cloud.database().collection('activity_register').add({
+        data: {
+          employeeID: userInfo.employeeID,
+          name: userInfo.name,
+          phoneNum: userInfo.phoneNum,
+          activity_type: activityTypes[selectedActivityType]
+        }
+      });
+      
+      wx.showModal({
+        title: '成功',
+        content: '报名成功',
+        showCancel: false
+      });
+    } catch (err) {
+      wx.showToast({ title: '提交失败', icon: 'none' });
+    }
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
